9584eba553e77ae34db4e2f28ae352d7146c3ada
Joren <joren.synaeve@norriq.com>
2022-01-13 16:22:32 +0100

    Add missing pages

---
+++ pages/kleinpeerke.vue
@@ -0,0 +1,10 @@
+<template>
+  <p>Not available</p>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "nuxt-property-decorator";
+@Component({})
+export default class KleinPeerke extends Vue {
+}
+</script>
