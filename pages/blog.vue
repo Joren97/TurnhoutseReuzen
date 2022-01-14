f162efe6adf686b03b8464af023ab71d9585f502
Joren Synaeve <synaevejoren@gmail.com>
2022-01-14 16:17:59 +0100

    Add loadin

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,47 +1,75 @@
 <template>
   <div class="section blog-posts">
     <div class="container">
-      <div class="columns" v-if="latestPost">
+      <div class="columns">
         <div class="column is-12 post">
           <article class="columns featured is-multiline">
             <div class="column is-7 post-img">
-              <img :src="latestPost.acf.afbeelding" />
+              <b-skeleton v-if="loadingActive" height="100px"></b-skeleton>
+              <img v-if="showContent" :src="latestPost.acf.afbeelding" />
             </div>
             <div class="column is-5">
               <div>
                 <h3 class="heading post-category">
-                  {{ latestPost.acf.categorie.name }}
+                  <b-skeleton v-if="loadingActive" width="80px"></b-skeleton>
+                  <span v-if="showContent">
+                    {{ latestPost.acf.categorie.name }}</span
+                  >
                 </h3>
                 <h1 class="title post-title">
-                  {{ latestPost.title.rendered }}
+                  <b-skeleton
+                    v-if="loadingActive"
+                    width="160px"
+                    height="30px"
+                  ></b-skeleton>
+                  <span v-if="showContent">
+                    {{ latestPost.title.rendered }}</span
+                  >
                 </h1>
+                <p v-if="loadingActive"><text-loader /></p>
                 <p
+                  v-if="showContent"
                   class="post-excerpt"
                   v-html="shortContent(latestPost, 400)"
                 ></p>
                 <br />
-                <a href="#" class="button is-primary">Lees meer</a>
+                <a href="#" class="button is-primary" v-if="showContent"
+                  >Lees meer</a
+                >
               </div>
             </div>
           </article>
         </div>
       </div>
-      <hr />
+      <hr v-if="showContent" />
       <div class="columns is-multiline">
         <div class="column is-4 post" v-for="p in posts" :key="p.id">
           <article class="columns is-multiline">
             <div class="column is-12 post-img">
-              <img :src="p.acf.afbeelding" alt="Featured Image" />
+              <b-skeleton v-if="loadingActive" height="60px"></b-skeleton>
+              <img v-else :src="p.acf.afbeelding" alt="Featured Image" />
             </div>
             <div class="column is-12 featured-content">
-              <h3 class="heading post-category">{{ p.acf.categorie.name }}</h3>
-              <h1 class="title post-title">{{ p.title.rendered }}</h1>
+              <b-skeleton v-if="loadingActive" width="80px"></b-skeleton>
+              <h3 v-else class="heading post-category">
+                {{ p.acf.categorie.name }}
+              </h3>
+              <b-skeleton
+                v-if="loadingActive"
+                width="130px"
+                height="20px"
+              ></b-skeleton>
+              <h1 v-else class="title post-title">{{ p.title.rendered }}</h1>
+              <p v-if="loadingActive"><text-loader /></p>
               <p
+                v-else
                 class="post-excerpt"
                 v-html="shortContent(latestPost, 180)"
               ></p>
               <br />
-              <a href="#" class="button is-primary">Lees meer</a>
+              <a href="#" class="button is-primary" v-if="!loadingActive"
+                >Lees meer</a
+              >
             </div>
           </article>
         </div>
@@ -65,6 +93,14 @@ export default class Blog extends Vue {
 
   get latestPost() {
     return postModule.items[0];
+  }
+
+  get loadingActive() {
+    return postModule.loading;
+  }
+
+  get showContent() {
+    return !this.loadingActive && this.latestPost;
   }
 
   shortContent(post: any, length: number) {
